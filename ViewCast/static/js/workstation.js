c688c613bb06d9e3a1d8851fada091ec0384b3cd
Jean Schwickerath <j_schwickerath@hotmail.com>
2022-08-08 16:19:49 +0200

    add media table and drag and copy for create flux

--- ViewCast/static/js/workstation.js
+++ ViewCast/static/js/workstation.js
@@ -5,3 +5,20 @@ function openForm() {
 function closeForm() {
     document.getElementById("myForm").style.display = "none";
 }
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data).cloneNode(true));
+    var row = document.getElementById("myRow");
+    var x = row.insertCell(-1);
+    x.innerHTML = "<width='200px'>";
+}
--- ViewCast/views/views/views.py
+++ ViewCast/views/views/views.py
@@ -17,6 +17,8 @@ if len(ALLOWED_EXTENSIONS) < 1:
     current_app.logger.warning(f"No media extension find")
 
 
+# region Route
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     form = IndexForm()
@@ -45,8 +47,6 @@ def workstation():
     if not session.get("name"):
         current_app.logger.warning(f"User not connected")
         return redirect("/login/")
-    if len(media_service.get_list_medias_name_by_id_user(session.get("id_user"))) < 1:
-        current_app.logger.warning(f"No Media")
     form = WorkstationForm()
     if request.method == 'POST':
         current_app.logger.info(f"Add media")
@@ -61,11 +61,17 @@ def workstation():
             _new_media = media_service.new_media(media.filename, filename.rsplit('.', 1)[1].lower(), 1)
             if _new_media is None:
                 current_app.logger.warning(f"Error update with {filename}")
+    lst_media = media_service.get_list_medias_name_by_id_user(session.get("id_user"))
+    matrice_media = [lst_media[_media:_media + 5] for _media in range(0, len(lst_media), 5)]
+    if len(lst_media) < 0:
+        current_app.logger.warning(f"No Media")
+    return render_template("workstation.html", form=form, name=session.get("name"), table=matrice_media)
 
-    return render_template("workstation.html", form=form, name=session.get("name"))
+# endregion
 
-
+# region function
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+# endregion
