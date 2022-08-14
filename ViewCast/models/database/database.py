c1c851969740f4c7267e18e360f6c6a411e7466e
JohnLest <j_schwickerath@hotmail.com>
2022-08-14 18:15:41 +0200

    do to docker

--- ViewCast/models/database/database.py
+++ ViewCast/models/database/database.py
@@ -2,5 +2,5 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-engine = create_engine("mariadb+mariadbconnector://viewcast:V13w-C4st@192.168.0.101:3307/viewcast")
+engine = create_engine("mariadb+mariadbconnector://viewcast:V13w-C4st@192.168.0.110:3306/viewcast")
 Session = sessionmaker(bind=engine)
